5d2d17775fd1122c6c4afcb252f5f3190fa70c94
git-jxj <1398349502@qq.com>
2020-05-25 16:50:50 +0800

    11

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM openjdk:14-jdk
-ARG JARFILE
-COPY ${JARFILE} app.jar
-EXPOSE 8080
-ENTRYPOINT ["java","-jar","/app.jar"]
+#FROM openjdk:14-jdk
+#ARG JARFILE
+#COPY ${JARFILE} app.jar
+#EXPOSE 8080
+#ENTRYPOINT ["java","-jar","/app.jar"]
+FROM hello-world
--- src/main/java/com/jxj/demo/controller/TestConrtoller.java
+++ src/main/java/com/jxj/demo/controller/TestConrtoller.java
@@ -1,8 +1,7 @@
 package com.jxj.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "test")
@@ -11,5 +10,11 @@ public class TestConrtoller {
     @ResponseBody
     public String test() {
         return "访问成功-------0523";
+    }
+
+    @RequestMapping(value = "test1")
+    @ResponseBody
+    public String test1(@RequestParam("name") String name, @RequestParam("pwd") String pwd) {
+        return name + ":" + pwd;
     }
 }
